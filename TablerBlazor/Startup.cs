0c560bfaa6e235de11e85bf60904c18ad2aebfaa
Saurabh Sharma <04srs1992@gmail.com>
2021-03-13 10:25:02 +0530

    Pages Add

--- TablerBlazor/Startup.cs
+++ TablerBlazor/Startup.cs
@@ -1,3 +1,6 @@
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +24,12 @@ namespace TablerBlazor
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddBlazorise(options =>
+     {
+         options.ChangeTextOnKeyPress = true; // optional
+      })
+     .AddBootstrapProviders()
+     .AddFontAwesomeIcons();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
